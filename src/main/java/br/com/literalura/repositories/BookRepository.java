1fe3b9c0d8b549639e4c3a504b9bcffcfd2a617c
maxjdev <maxjramosdev@gmail.com>
2024-11-17 17:19:46 -0300

    feat: add jpa repositories

---
+++ src/main/java/br/com/literalura/repositories/BookRepository.java
@@ -0,0 +1,14 @@
+package br.com.literalura.repositories;
+
+import br.com.literalura.domain.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByTitleContains(String title);
+    List<Book> findByLanguageContains(String language);
+}
